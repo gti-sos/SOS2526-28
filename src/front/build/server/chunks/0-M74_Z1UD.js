d58591c3f359078c464bd161085a7594a87e1507
AlbertoJimenezMateos <aljima15@gmail.com>
2026-03-23 02:21:18 +0100

    Fix CI/CD: Configurado Render para compilar en la nube y eliminada carpeta build

--- src/front/build/server/chunks/0-M74_Z1UD.js
+++
@@ -1,9 +0,0 @@
-const index = 0;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_layout.svelte-BO-PRQmW.js')).default;
-const imports = ["_app/immutable/nodes/0.BD-xs_mS.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/Ao8hT-MN.js","_app/immutable/chunks/N_yu8ttR.js","_app/immutable/chunks/CLsQXN_c.js","_app/immutable/chunks/DYsoKMlX.js"];
-const stylesheets = ["_app/immutable/assets/0.CQK_qQCO.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=0-M74_Z1UD.js.map
