db970b2c73e7fd31b1476b981dcdbd83885ec2af
AlbertoJimenezMateos <aljima15@gmail.com>
2026-03-23 15:17:54 +0100

    Entrega final de Alberto: CRUD completo, frontend, Postman y diseño adaptado

--- src/front/src/routes/+layout.svelte
+++ src/front/src/routes/+layout.svelte
@@ -9,7 +9,7 @@
             <nav>
                 <a href="/" class="nav-btn">🏠 INICIO</a>
                 <a href="https://github.com/gti-sos/SOS2526-28" target="_blank" class="nav-btn">📁 REPOSITORIO</a>
-                <a href="/about" class="nav-btn">ℹ️ ABOUT</a>
+                <a href="/about" class="nav-btn">ℹ️ NOSOTROS</a>
             </nav>
         </div>
     </header>
