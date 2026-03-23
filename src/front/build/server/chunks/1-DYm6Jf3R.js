aeb41904e3d6c5c34ad99b961f57418d4b7c78e7
jorgedx2 <jorgedx2@yahoo.com>
2026-03-23 13:19:42 +0100

    gitignore db

--- src/front/build/server/chunks/1-DYm6Jf3R.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-BtR2z48S.js')).default;
-const imports = ["_app/immutable/nodes/1.DISJtH1e.js","_app/immutable/chunks/gBrvu3bH.js","_app/immutable/chunks/C-zfE2iW.js","_app/immutable/chunks/Cfug8aQt.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-DYm6Jf3R.js.map
--- src/front/build/server/chunks/6-B8rQNdQe.js
+++
@@ -1,9 +0,0 @@
-const index = 6;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-DYiqdgtA.js')).default;
-const imports = ["_app/immutable/nodes/6.EyrE3O5A.js","_app/immutable/chunks/gBrvu3bH.js","_app/immutable/chunks/Cfug8aQt.js"];
-const stylesheets = ["_app/immutable/assets/6.BtGwxddL.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=6-B8rQNdQe.js.map
